91b41a9cb5c928a6ce71c1434797b1a42c7efc6e
tanmichael1 <michaelrstan@gmail.com>
2021-10-02 17:58:18 +1300

    Begin to make page mobile friendly

--- src/components/footer/Tabs.jsx
+++ src/components/footer/Tabs.jsx
@@ -3,33 +3,34 @@ import React from "react";
 function Tabs({ changeFilter, filter }) {
   return (
     <div className="tabs">
-      <h1>Filter </h1>
-      <button
-        onClick={() => changeFilter("All Items")}
-        defaultValue="true"
-        type="button"
-        style={{ flex: 1 }}
-        className="btn btn-info"
-      >
-        All Items
-      </button>{" "}
-      <button
-        onClick={() => changeFilter("Active")}
-        type="button"
-        style={{ flex: 1 }}
-        className="btn btn-info"
-      >
-        Active
-      </button>{" "}
-      <button
-        onClick={() => changeFilter("Completed")}
-        type="button"
-        style={{ flex: 1 }}
-        className="btn btn-info"
-      >
-        Completed
-      </button>
-      <br /> <br />
+      <h1 className="tabsHeader">Filter </h1>
+      <div className="tabButtons">
+        <button
+          onClick={() => changeFilter("All Items")}
+          defaultValue="true"
+          type="button"
+          style={{ flex: 1 }}
+          className="btn btn-info tabButton"
+        >
+          All Items
+        </button>{" "}
+        <button
+          onClick={() => changeFilter("Active")}
+          type="button"
+          style={{ flex: 1 }}
+          className="btn btn-info tabButton"
+        >
+          Active
+        </button>{" "}
+        <button
+          onClick={() => changeFilter("Completed")}
+          type="button"
+          style={{ flex: 1 }}
+          className="btn btn-info tabButton"
+        >
+          Completed
+        </button>
+      </div>
       <p> Current filter: {filter}</p>
     </div>
   );
